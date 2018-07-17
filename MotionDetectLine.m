1053940941cfa631c89a41cb4fbfaa0aaad3a290
Jofred Cayabyab <jjay.cayabyab@gmail.com>
2018-07-17 11:29:51 -0600

    Added some comments

--- MotionDetectLine.m
+++ MotionDetectLine.m
@@ -7,8 +7,11 @@ clear;
 video = getVideo();
 vidLine = transformVideo(video);
 displacement = calculateDisplacements(vidLine);
+freq = fft(displacement);
 
 %Display output
 imshow(vidLine, [0 255]);
 figure;
 plot(displacement);
+figure;
+plot(freq);
